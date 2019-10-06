c542e24b788906d48a63d3b9b855dbb97d2d55f6
thalleslmf <lopthalles@gmail.com>
2019-10-06 18:07:11 -0300

    alterações

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -19,6 +19,137 @@ class Posts extends Component {
               1500s, when an unknown printer took a galley of type and scrambled
               it to make a type specimen book
             </p>
+            <i class="fa fa-user">Thalles Freitas </i>
+            <i class="fa fa-date">06/10/2019</i>
+            <br />
+            <Link to="/contact" class="square_btn">
+              Leia Mais
+            </Link>
+          </div>
+          <br />
+          <div class="post-preview">
+            <img src="/download.jpg" alt="" srcset="" />
+            <h2>
+              <a href="article">Teste </a>
+            </h2>
+            <p>
+              simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled
+              it to make a type specimen book
+            </p>
+
+            <Link to="/contact" class="square_btn">
+              Leia Mais
+            </Link>
+          </div>
+          <div class="post-preview">
+            <img src="\bxblue-emprestimo.png" alt="" srcset="" />
+            <h2>
+              <a href="article">Venha fazer empréstimo com a bxblue </a>
+            </h2>
+            <p>
+              simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled
+              it to make a type specimen book
+            </p>
+
+            <Link to="/contact" class="square_btn">
+              Leia Mais
+            </Link>
+          </div>
+          <div class="post-preview">
+            <img src="\bxblue-emprestimo.png" alt="" srcset="" />
+            <h2>
+              <a href="article">Venha fazer empréstimo com a bxblue </a>
+            </h2>
+            <p>
+              simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled
+              it to make a type specimen book
+            </p>
+
+            <Link to="/contact" class="square_btn">
+              Leia Mais
+            </Link>
+          </div>
+          <div class="post-preview">
+            <img src="\bxblue-emprestimo.png" alt="" srcset="" />
+            <h2>
+              <a href="article">Venha fazer empréstimo com a bxblue </a>
+            </h2>
+            <p>
+              simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled
+              it to make a type specimen book
+            </p>
+
+            <Link to="/contact" class="square_btn">
+              Leia Mais
+            </Link>
+          </div>
+          <div class="post-preview">
+            <img src="\bxblue-emprestimo.png" alt="" srcset="" />
+            <h2>
+              <a href="article">Venha fazer empréstimo com a bxblue </a>
+            </h2>
+            <p>
+              simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled
+              it to make a type specimen book
+            </p>
+
+            <Link to="/contact" class="square_btn">
+              Leia Mais
+            </Link>
+          </div>
+          <div class="post-preview">
+            <img src="\bxblue-emprestimo.png" alt="" srcset="" />
+            <h2>
+              <a href="article">Venha fazer empréstimo com a bxblue </a>
+            </h2>
+            <p>
+              simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled
+              it to make a type specimen book
+            </p>
+
+            <Link to="/contact" class="square_btn">
+              Leia Mais
+            </Link>
+          </div>
+          <div class="post-preview">
+            <img src="\bxblue-emprestimo.png" alt="" srcset="" />
+            <h2>
+              <a href="article">Venha fazer empréstimo com a bxblue </a>
+            </h2>
+            <p>
+              simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled
+              it to make a type specimen book
+            </p>
+
+            <Link to="/contact" class="square_btn">
+              Leia Mais
+            </Link>
+          </div>
+          <div class="post-preview">
+            <img src="\bxblue-emprestimo.png" alt="" srcset="" />
+            <h2>
+              <a href="article">Venha fazer empréstimo com a bxblue </a>
+            </h2>
+            <p>
+              simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled
+              it to make a type specimen book
+            </p>
 
             <Link to="/contact" class="square_btn">
               Leia Mais
