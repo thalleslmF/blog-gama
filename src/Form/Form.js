ccf14920d8b32c851e950dba20197488de97e03f
thalleslmf <lopthalles@gmail.com>
2019-10-07 00:55:02 -0300

    alterações

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -7,46 +7,44 @@ class Form extends Component {
       <div class="container-fluid">
         <div className="sidebar"></div>
         <div className="postzao">
-          <div class="post-preview">
-            <div className="container">
-              <div className="row">
-                <div className="col-sm-12 col-md-6 ">
-                  <h2 className="" id="header-blue">
-                    Entre em contato para as melhores ofertas
-                  </h2>
-                  <p>
-                    Inscreva-se com seu nome e email para poder acompanhar as
-                    melhores ofertas e propostas disponiveis!
-                  </p>
-                </div>
-                <div class="col-sm-12 col-md-6 " id="shadow">
-                  <form>
-                    <h3>Cadastro</h3>
-                    <div className="form-group">
-                      <div className="prepend">
-                        <i className="prepend"></i>
-                        <label>Nome</label>
-                        <input
-                          placeholder="Nome Completo"
-                          type="text"
-                          className="form-control"
-                        />
-                      </div>
+          <div className="container">
+            <div className="row" id="alinha-itens">
+              <div className="col-sm-12 col-md-6 ">
+                <h2 className="" id="header-blue">
+                  Entre em contato para as melhores ofertas
+                </h2>
+                <p>
+                  Inscreva-se com seu nome e email para poder acompanhar as
+                  melhores ofertas e propostas disponiveis!
+                </p>
+              </div>
+              <div class="col-sm-12 col-md-6 " id="shadow">
+                <form>
+                  <h3>Cadastro</h3>
+                  <div className="form-group">
+                    <div className="prepend">
+                      <i className="prepend"></i>
+                      <label>Nome</label>
+                      <input
+                        placeholder="Nome Completo"
+                        type="text"
+                        className="form-control"
+                      />
                     </div>
-                    <div className="form-group">
-                      <div className="prepend">
-                        <label>Email</label>
-                        <i className="prepend"></i>
-                        <input
-                          placeholder="Email"
-                          type="text"
-                          className="form-control"
-                        />
-                      </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="prepend">
+                      <label>Email</label>
+                      <i className="prepend"></i>
+                      <input
+                        placeholder="Email"
+                        type="text"
+                        className="form-control"
+                      />
                     </div>
-                    <button class="btn btn-primary">Enviar</button>
-                  </form>
-                </div>
+                  </div>
+                  <button class="btn btn-primary">Enviar</button>
+                </form>
               </div>
             </div>
           </div>
