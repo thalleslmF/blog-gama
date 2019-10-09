17d495cce94ef14f3f47cbba22c55fcf4d545e28
thalleslmf <lopthalles@gmail.com>
2019-10-09 18:49:55 -0300

    img responsive

--- src/Posts/Posts-copy.js
+++ src/Posts/Posts-copy.js
@@ -8,7 +8,7 @@ class Posts extends Component {
       <div>
         <div class="container">
           <div class="offset-md-1 col-md-10">
-            <header class="header">
+            <header class="header img-responsive">
               <div class="container">
                 <div class="row">
                   <div class="col-lg-8 col-md-10 mx-auto"></div>
