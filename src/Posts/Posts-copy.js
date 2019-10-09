f637ac703b0ec08da0c05bf70b33192898000066
thalleslmf <lopthalles@gmail.com>
2019-10-09 15:07:49 -0300

    redesign

---
+++ src/Posts/Posts-copy.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import "./Posts.css";
+
+import { Link, Router } from "react-router-dom";
+class Posts extends Component {
+  render() {
+    return (
+      <div>
+        <div class="offset-md-2 col-md-8">
+          <header class="masthead">
+            <div class="overlay"></div>
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-8 col-md-10 mx-auto">
+                  <div class="site-heading">
+                    <h1>Clean Blog</h1>
+                    <span class="subheading">
+                      A Blog Theme by Start Bootstrap
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </header>
+        </div>
+        <div className="postzao">
+          <h1>Posts recentes</h1>
+
+          <div className="post-preview">
+            <div className="col-md-12">
+              <div class="row">
+                <div class=" offset-md-2 col-md-8"></div>
+                <div className="offset-md-2 col-md-8">
+                  <div className="row">
+                    <h3>
+                      <a href="article">Venha fazer empréstimo com a bxblue </a>
+                    </h3>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-12">
+                <div className="row">
+                  <div className="col-md-6 col-sm-12">
+                    <i className="fa fa-user">Thalles Freitas </i>
+                  </div>
+                  <div className="col-md-6 col-sm-12">
+                    <i className="fa fa-calendar"> 06/10/2019</i>
+                  </div>
+                </div>
+              </div>
+              <hr></hr>
+              <div class="row" id="artigo">
+                <p>
+                  simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industry's standard dummy text ever
+                  since the 1500s, when an unknown printer took a galley of type
+                  and scrambled it to make a type specimen book
+                </p>
+
+                <br />
+                <div className="col-md-12">
+                  <Link to="/article" className="btn btn-info btn-block">
+                    Leia Mais
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+        </div>
+
+        <div className="sidebar1"></div>
+      </div>
+    );
+  }
+}
+export default Posts;
