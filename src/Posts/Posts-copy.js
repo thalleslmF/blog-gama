0627b91ff731a3afeca8fec436493010e0dcaad2
thalleslmf <lopthalles@gmail.com>
2019-10-09 15:58:42 -0300

    novo layout

--- src/Posts/Posts-copy.js
+++ src/Posts/Posts-copy.js
@@ -42,17 +42,19 @@ class Posts extends Component {
                     </div>
                   </div>
                   <hr></hr>
-
-                  <p>
-                    simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever
-                    since the 1500s, when an unknown printer took a galley of
-                    type and scrambled it to make a type specimen book
-                  </p>
+                  <div class="row">
+                    <p>
+                      simply dummy text of the printing and typesetting
+                      industry. Lorem Ipsum has been the industry's standard
+                      dummy text ever since the 1500s, when an unknown printer
+                      took a galley of type and scrambled it to make a type
+                      specimen book
+                    </p>
+                    <Link to="/article" className="btn btn-dark btn-block">
+                      Leia Mais
+                    </Link>
+                  </div>
                 </div>
-                <Link to="/article" className="btn btn-dark btn-block">
-                  Leia Mais
-                </Link>
               </div>
             </div>
           </div>
